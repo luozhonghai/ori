b04156c269667bc01da470219890803f99f95b15
luozhonghai <412865725@qq.com>
2015-04-09 15:16:22 +0900

    init turntable devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  turntable :user_cluster, :id
+  
+  sequencer :user_seq
+end
